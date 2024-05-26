aa6a3c0804c42f868e1850ccdfaf23a5cca34b3e
arvoglade <arvoglade@gmail.com>
2024-05-26 18:58:46 +0500

    Swagger

---
+++ src/main/java/ru/gashev/microservice/example/config/swagger/SwaggerInfo.java
@@ -0,0 +1,14 @@
+package ru.gashev.microservice.example.config.swagger;
+
+public class SwaggerInfo {
+
+    /**
+     * Название API.
+     */
+    public static final String TITLE = "Project manager";
+
+    /**
+     * Описание API.
+     */
+    public static final String DESCRIPTION = "Project manager";
+}
